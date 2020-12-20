151abd7788e8171ec53e74d65baa7a8c8b4f308d
Mehdi <mehdoop11@gmail.com>
2020-12-20 13:21:35 +0100

    script added

---
+++ WPFW-week15/wwwroot/js/student.js
@@ -0,0 +1,21 @@
+﻿function getStudent()
+{
+    fetch('/GetStudent?id=' + document.getElementById("studentList").value)
+        .then(res => res.json())
+        .then(stud => document.getElementById('studs').innerHTML += stud.Name + " is " + stud.Age + " years old")
+        .catch(e => document.getElementById("errorMelding").innerHTML = e);
+}
+
+function getStudents() {
+    fetch('/Home/GetStudents')
+        .then(res => res.json())
+        .then(stud => {
+            for (const s of stud) {
+                let option = document.createElement("option");
+                option.value = s.Id;
+                option.text = s.Name;
+                document.getElementById("studentList").appendChild(option);
+            }
+        })
+        .catch(e => document.getElementById("errorMelding").innerHTML = e);
+}
